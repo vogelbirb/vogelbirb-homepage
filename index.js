ab985f08babcf3b67383de06f2af5c7257bc760c
Matheus L. P. <contact@matheuslp.com>
2026-02-18 20:03:29 -0600

    fix: change deafult port to 3000

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.static("public"));
 
-const port = 3001;
+const port = 3000;
 app.listen(process.env.PORT || port, "0.0.0.0");
 console.log(`Server listening on port ${port}`);
