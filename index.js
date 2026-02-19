9586413cb36a2a74177c2d0c2df20b98e757189b
Matheus L. P. <contact@matheuslp.com>
2026-02-18 19:55:08 -0600

    fix: bind to 0.0.0.0

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const app = express();
 app.use(express.static("public"));
 
 const port = 3001;
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || port, "0.0.0.0");
+console.log(`Server listening on port ${port}`);
