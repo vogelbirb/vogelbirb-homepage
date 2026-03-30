30ece3f03cd046a02e4890d4965435cc4f5a6db7
VogelBirb <me@vogelbirb.com>
2026-03-29 20:04:18 -0500

    feat: remove Express.js server

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:24
-WORKDIR /usr/src/vogelbirb-homepage
-
-COPY . .
-
-RUN npm install
-
-EXPOSE 3000
-CMD ["npm", "start"]
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.use(express.static("public"));
-
-const port = 3000;
-app.listen(process.env.PORT || port, "0.0.0.0");
-console.log(`Server listening on port ${port}`);
